003b0e739a1aa6e9e08ac839ea2a77a6631f6d48
leonan123 <leonanorth3@gmail.com>
2024-05-21 01:20:38 -0300

    feat: add request password recover route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -15,6 +15,7 @@ import { errorHandler } from './error-handler'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -48,6 +49,7 @@ app.register(fastifyCors)
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
+app.register(requestPasswordRecover)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🔥 HTTP server running in http://localhost:3333')
