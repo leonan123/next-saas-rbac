f15fc0eba1ebcf9396a93f19304dba813124a563
leonan123 <leonanorth3@gmail.com>
2024-05-21 01:03:18 -0300

    first commit

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,245 @@
+import { faker } from '@faker-js/faker'
+import { PrismaClient } from '@prisma/client'
+import { hash } from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+async function seed() {
+  const deleteAllOrganizations = prisma.organization.deleteMany()
+  const deleteAllUsers = prisma.user.deleteMany()
+
+  await prisma.$transaction([deleteAllOrganizations, deleteAllUsers])
+
+  const passwordHash = await hash('123456', 1)
+
+  const user1 = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      avatarUrl: 'https://github.com/leonan123.png',
+      passwordHash,
+    },
+  })
+
+  const user2 = await prisma.user.create({
+    data: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      avatarUrl: faker.image.avatarGitHub(),
+      passwordHash,
+    },
+  })
+
+  const user3 = await prisma.user.create({
+    data: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      avatarUrl: faker.image.avatarGitHub(),
+      passwordHash,
+    },
+  })
+
+  await prisma.organization.create({
+    data: {
+      name: 'Acme Inc (Admin)',
+      domain: 'acme.com',
+      slug: 'acme-admin',
+      avatarUrl: faker.image.avatarGitHub(),
+      shouldAttachUsersByDomain: true,
+      ownerId: user1.id,
+      projects: {
+        createMany: {
+          data: [
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+          ],
+        },
+      },
+      members: {
+        createMany: {
+          data: [
+            {
+              userId: user1.id,
+              role: 'ADMIN',
+            },
+            {
+              userId: user2.id,
+              role: 'MEMBER',
+            },
+            {
+              userId: user3.id,
+              role: 'MEMBER',
+            },
+          ],
+        },
+      },
+    },
+  })
+
+  await prisma.organization.create({
+    data: {
+      name: 'Acme Inc (Member)',
+      slug: 'acme-member',
+      avatarUrl: faker.image.avatarGitHub(),
+      ownerId: user1.id,
+      projects: {
+        createMany: {
+          data: [
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+          ],
+        },
+      },
+      members: {
+        createMany: {
+          data: [
+            {
+              userId: user1.id,
+              role: 'MEMBER',
+            },
+            {
+              userId: user2.id,
+              role: 'ADMIN',
+            },
+            {
+              userId: user3.id,
+              role: 'MEMBER',
+            },
+          ],
+        },
+      },
+    },
+  })
+
+  await prisma.organization.create({
+    data: {
+      name: 'Acme Inc (Billing)',
+      slug: 'acme-billing',
+      avatarUrl: faker.image.avatarGitHub(),
+      ownerId: user1.id,
+      projects: {
+        createMany: {
+          data: [
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+            {
+              name: faker.lorem.words(5),
+              slug: faker.lorem.slug(5),
+              description: faker.lorem.paragraph(),
+              avatarUrl: faker.image.avatarGitHub(),
+              ownerId: faker.helpers.arrayElement([
+                user1.id,
+                user2.id,
+                user3.id,
+              ]),
+            },
+          ],
+        },
+      },
+      members: {
+        createMany: {
+          data: [
+            {
+              userId: user1.id,
+              role: 'BILLING',
+            },
+            {
+              userId: user2.id,
+              role: 'ADMIN',
+            },
+            {
+              userId: user3.id,
+              role: 'MEMBER',
+            },
+          ],
+        },
+      },
+    },
+  })
+}
+
+seed().then(() => {
+  console.log('💫 Database seeded!')
+})
--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,12 +1,19 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
+        tags: ['auth'],
+        summary: 'Create a new account',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -14,8 +21,46 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'user created'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        throw new BadRequestError('user with same email already exists.')
+      }
+
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+          memberOn: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
