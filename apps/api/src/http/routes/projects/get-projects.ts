864c2c78623b3322298b10302bd08cb6de9bd811
leonan123 <leonanorth3@gmail.com>
2024-06-20 17:59:08 -0300

    feat: add routes to get one or many projects

---
+++ apps/api/src/http/routes/projects/get-projects.ts
@@ -0,0 +1,89 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getProjects(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Get all organizations projects.',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              projects: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  description: z.string(),
+                  name: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.string().nullable(),
+                  organizationId: z.string().uuid(),
+                  ownerId: z.string().uuid(),
+                  createdAt: z.date(),
+                  owner: z.object({
+                    id: z.string().uuid(),
+                    name: z.string().nullable(),
+                    avatarUrl: z.string().nullable(),
+                  }),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const userId = membership.userId
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Project')) {
+          throw new UnauthorizedError(
+            "You're not allowed to see organization projects.",
+          )
+        }
+
+        const projects = await prisma.project.findMany({
+          select: {
+            id: true,
+            description: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            organizationId: true,
+            ownerId: true,
+            createdAt: true,
+            owner: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(200).send({ projects })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -28,6 +28,8 @@ import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -87,6 +89,8 @@ app.register(transferOrganization)
 // -------- ORGANIZATIONS - PROJECTS --------
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`🔥 HTTP server running in http://localhost:${env.SERVER_PORT}`)
